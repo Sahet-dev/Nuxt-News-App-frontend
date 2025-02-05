676b497a23395b3d288fff1bc764a03cd30ca84a
Sahetmyrat <kakajansahy@gmail.com>
2025-02-05 12:12:36 +0200

    Change site description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,8 +35,13 @@ export default defineNuxtConfig({
       title: 'Döwür News',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'A modern news application built with Nuxt.' },
-        { name: 'google-site-verification', content: '7DU3PTICXXaok5RwiA02nHDmB53ZWR2oejJioBr2NRw' }
+        { name: 'description', content: 'Döwrümiziň möhüm wakalaryň yzgiderliligi.' },
+        { name: 'google-site-verification', content: '7DU3PTICXXaok5RwiA02nHDmB53ZWR2oejJioBr2NRw' },
+        { property: 'og:title', content: 'Döwür News' },
+        { property: 'og:description', content: 'Döwrümiziň möhüm wakalaryň yzgiderliligi.' },
+        { property: 'og:image', content: '/share.png' },  // Open Graph image
+        { name: 'twitter:image', content: '/share.png' },
+        { property: 'og:url', content: 'https://www.dowur-news.com' },
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
